c58d048e8bf3ca302f56060f5bb3cd71875c2a98
MotiAtiya <a.moti96@gmail.com>
2023-08-01 04:10:06 +0300

    add new files

---
+++ src/Colors.cpp
@@ -0,0 +1,9 @@
+#include "Colors.h"
+
+sf::Color getColor(size_t index)
+{
+  static std::array ballColors = {sf::Color::Red, sf::Color::Blue, sf::Color::Yellow, sf::Color::Green};
+  if (index >= ballColors.size())
+    throw std::out_of_range("ratio illegal\n");
+  return ballColors[index];
+}
